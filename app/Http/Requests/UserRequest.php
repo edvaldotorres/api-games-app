4872e7988c389cd40aa07ed8339af2816003b2b9
Edvaldo Torres <edvaldodev@gmail.com>
2022-05-25 02:41:27 -0300

    fix: correction request validated user

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\Enums\UserType;
+
 class UserRequest extends BaseRequest
 {
     /**
@@ -11,11 +13,28 @@ class UserRequest extends BaseRequest
      */
     public function rules()
     {
-        return [
+        $return = [
             'name' => 'required|string|max:255',
             'email' => 'required|string|email|max:255|unique:users',
             'password' => 'required|string|min:6',
             'is_admin' => 'required|in:0,1',
         ];
+
+        if (
+            $this->route()->uri == 'api/register' ||
+            ($this->route()->uri == 'api/users/{id}' && auth('api')->user()->is_admin != UserType::ADMIN)
+        ) {
+            unset($return['is_admin']);
+        }
+
+        if ($this->route()->uri == 'api/users/{id}') {
+            $return['name'] = 'string|max:255';
+            $return['email'] = 'string|email|max:255';
+            $return['password'] = 'string|min:6';
+            if (auth('api')->user()->is_admin)
+                $return['is_admin'] = 'in:0,1';
+        }
+
+        return $return;
     }
 }
