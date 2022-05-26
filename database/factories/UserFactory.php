4d406db24bf2c613d81c97531d8291126d859e2c
Edvaldo Torres <edvaldodev@gmail.com>
2022-05-26 05:19:10 -0300

    feat user padrão seed

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,10 +18,11 @@ class UserFactory extends Factory
     public function definition()
     {
         return [
-            'name' => $this->faker->name(),
-            'email' => $this->faker->unique()->safeEmail(),
+            'name' => 'Developer',
+            'email' => 'developer@dev.com.br',
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => 'password',
+            'is_admin' => true,
             'remember_token' => Str::random(10),
         ];
     }
